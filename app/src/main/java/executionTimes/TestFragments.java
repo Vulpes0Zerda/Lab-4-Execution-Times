9fc25d8378325deb931da1da197f42e5ace99960
JasBi05 <jassi300805@gmail.com>
2025-05-16 16:17:50 +0200

    Tested all Fragments

--- app/src/main/java/executionTimes/TestFragments.java
+++ app/src/main/java/executionTimes/TestFragments.java
@@ -8,19 +8,50 @@ public class TestFragments {
         long t1 = System.nanoTime();
 
         int sum = 0;
-        int n = 999999999;
+        long steps= 0;
+        int n = 1000000;
 
 
         //Beginn of Fragments//
-        // Fragment #1
+        /*// Fragment #1
         for (int i = 0; i < n; i++) {
-            sum++;
-        }
+            //steps++;
 
+            sum++;
+
+           steps++;
+        }*/
+         
+
+         //Fragment #2
+        /*for ( int i = 0; i < n; i ++){
+            //steps++;
+            for ( int j = 0; j < n; j ++){
+                sum++;
+                    steps++;
+            }
+        }*/
+        /*// Fragment #3
+        for ( int i = 0; i < n; i ++){
+            for ( int j = i; j < n; j ++){
+                sum++;
+                    steps++;
+            }
+        }*/
+        // Fragment #4
+        for ( int i = 0; i < n; i ++){
+            sum ++;
+                steps++;
+            for ( int j = 0; j < n; j ++){
+                sum ++;
+                    steps++;
+            }
+        }
 
         long t2 = System.nanoTime();   // End time measurement
 
-        System.out.println("Fragment #1: " + (t2 - t1) + " ns");
+        System.out.println("Fragment #4: " + (t2 - t1) + " ns");
+        System.out.println("Schritte: " + steps);
         System.out.println("In Sekunden: " + (t2 - t1) / 1000000000.0 + " s");
     }
 }
